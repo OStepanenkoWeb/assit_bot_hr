340becea792f24108c69b3411ddcb7846f88e6a0
Stepanenko Oleg <o.stepanenko@id-mt.ru>
2023-09-01 15:12:07 +0300

    fix errors update database

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 const bot = new TelegramApi(process.env.TOKEN_BOT, { polling: true })
 
-const testConnectDB = async () => {
+const connectDB = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
@@ -16,7 +16,7 @@ const testConnectDB = async () => {
     }
 }
 
-testConnectDB()
+connectDB()
 
 
 // Установка команд
@@ -28,25 +28,27 @@ bot.setMyCommands( [
 // Обработка сообщений
 bot.on( 'message',  async msg => {
     const text = msg.text
-    const chatId = msg.chat.id
-
+    const chatIdUsr = msg.chat.id
+    console.log('message')
 
     try {
-        const user = await UserModel.findOne({chatId})
-        const isAdmin = user.admin
+        const user = await UserModel.findOne({ where: { chatId: chatIdUsr }})
+        console.log(user)
+        const isAdmin = user && user.admin
 
-        if (user && user.order && !isAdmin) {
-            return await bot.sendMessage(chatId, 'Извините, но Ваша анкета уже была отправлена HR менеджеру :)')
+        if (user && user.order && user.chatId === chatIdUsr && !isAdmin) {
+            return await bot.sendMessage(chatIdUsr, 'Извините, но Ваша анкета уже была отправлена HR менеджеру :)')
         }
 
 
         if (text === '/start') {
+            console.log(isAdmin)
             if(!isAdmin) {
-                await UserModel.create({chatId})
+                await UserModel.create({chatId: chatIdUsr})
             }
 
-            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/897/df3/897df311-e19d-4a7d-8b27-4929abbcf2cc/1.webp')
-            await bot.sendMessage(chatId, 'Добро пожаловать в ассистент бот, который поможет Вам с оформлением в нашей компании:)' +
+            await bot.sendSticker(chatIdUsr, 'https://tlgrm.ru/_/stickers/897/df3/897df311-e19d-4a7d-8b27-4929abbcf2cc/1.webp')
+            await bot.sendMessage(chatIdUsr, 'Добро пожаловать в ассистент бот, который поможет Вам с оформлением в нашей компании:)' +
                 'Выберите, пожалуйста, предпочтительный способ оформления)', decorOptions)
             return
 
@@ -54,53 +56,58 @@ bot.on( 'message',  async msg => {
 
 
         if (text === '/info') {
-            return await bot.sendMessage(chatId, `${msg.from.first_name} ${msg.from.last_name} компания Ай-Ди Технологии управления занимается разработкой и внедрением собственного программного обеспечения`)
+            return await bot.sendMessage(chatIdUsr, `${msg.from.first_name} ${msg.from.last_name} компания Ай-Ди Технологии управления занимается разработкой и внедрением собственного программного обеспечения`)
         }
+
+        return await bot.sendMessage(chatIdUsr, 'Извините, но я пока не умею отвечать на незнакомые сообщения :(')
     } catch (e) {
         const users = await getAdmins()
 
         if (Array.isArray(users) && users.length) {
             users.every(async user => {
-                    await bot.sendMessage(user.chatId, `Произошла ошибка отправки сообщения: ${e}`)
+                    await bot.sendMessage(user.chatIdUsr, `Произошла ошибка отправки сообщения: ${e}`)
             })
         }
-    }
 
-    return await bot.sendMessage(chatId, 'Извините, но я пока не умею отвечать на незнакомые сообщения :(')
+        console.log(e)
+    }
 
 })
 
 // Обработка кнопок
 bot.on('callback_query', async msg => {
     const data = msg.data
-    const chatId = msg.message.chat.id
-    const candidate = await UserModel.findOne({ chatId })
+    const chatIdUsr = msg.message.chat.id
+    const candidate = await UserModel.findOne({ where: { chatId: chatIdUsr }})
 
-    candidate.userName = msg.from.username
-    candidate.firstName = msg.from.first_name
-    candidate.lastName = msg.from.last_name
-    await candidate.save()
+    if (candidate) {
+        candidate.userName = msg.from.username
+        candidate.firstName = msg.from.first_name
+        candidate.lastName = msg.from.last_name
+        await candidate.save()
+    }
+
 
     if(data === 'Офис Москва') {
         await bot.sendMessage(chatId, 'Ждем вас с нетерпением в нашем офисе по адресу' +
             '' +
             'г. Москва, ул. Дербеневская набережная д.11А')
         candidate.office = data
-        candidate.save()
+        await candidate.save()
 
         return
     }
 
     if(data === 'Удаленное') {
-        await bot.sendMessage(chatId, 'Какая техника Вам потребуется для работы?', techOptions)
+        await bot.sendMessage(chatIdUsr, 'Какая техника Вам потребуется для работы?', techOptions)
         candidate.office = data
-        candidate.save()
+        await candidate.save()
 
         return
     }
 
     if(['Нужен монитор и ноутбук', 'Нужен монитор', 'Нужен ноутбук', 'Не нужна'].includes(data)) {
-        await bot.sendMessage(chatId, 'Какая у вас трудовая книжка?', workBookOptions)
+        await bot.sendMessage(chatIdUsr, 'Какая у вас трудовая книжка?', workBookOptions)
         candidate.needTech = data
         await candidate.save()
 
@@ -112,11 +119,11 @@ bot.on('callback_query', async msg => {
         await candidate.save()
 
         if(data === 'Электронная трудовая') {
-            await bot.sendMessage(chatId, 'Зарплатный проект Альфа банк. Если ли у вас карта?', salaryBookMoreOptions)
+            await bot.sendMessage(chatIdUsr, 'Зарплатный проект Альфа банк. Если ли у вас карта?', salaryBookMoreOptions)
             return
         }
 
-        await bot.sendMessage(chatId, 'В каком виде хотите продолжить вести трудовую книжку?', workBookMoreOptions)
+        await bot.sendMessage(chatIdUsr, 'В каком виде хотите продолжить вести трудовую книжку?', workBookMoreOptions)
 
 
         return
@@ -125,7 +132,7 @@ bot.on('callback_query', async msg => {
     if(['Продолжить в электронной', 'Продолжить в бумажной'].includes(data)) {
         if ('Продолжить в электронной' === data) {
             await bot.sendMessage(
-                chatId,
+                chatIdUsr,
                 "<strong>Скачайте пожалуйста бланк заявления на переход ведения в электронную трудовую книжку, заполните и отправьте HR </strong>" +
                 "<a href='https://docs.google.com/document/d/1m_wPVDWLvDewPPl1iCVTiuzmwW-bBJ-L/edit?usp=sharing&ouid=109174770860598202689&rtpof=true&sd=true'>Заявление на электронную трудовую</a>",
                 {parse_mode: "HTML"}
@@ -134,7 +141,7 @@ bot.on('callback_query', async msg => {
             candidate.needWorkBook = data
         await candidate.save()
 
-        await bot.sendMessage(chatId, 'Зарплатный проект Альфа банк. Если ли у вас карта?', salaryBookMoreOptions)
+        await bot.sendMessage(chatIdUsr, 'Зарплатный проект Альфа банк. Если ли у вас карта?', salaryBookMoreOptions)
 
         return
     }
@@ -142,18 +149,18 @@ bot.on('callback_query', async msg => {
     if(['Есть Альфа счет', 'Нет Альфа счет'].includes(data)) {
 
         if ('Нет Альфа счет' === data) {
-            await bot.sendMessage(chatId, 'Скачайте пожалуйста бланк заявления на открытие счета в Альфа Банк,' +
+            await bot.sendMessage(chatIdUsr, 'Скачайте пожалуйста бланк заявления на открытие счета в Альфа Банк,' +
                 'заполните и отправьте HR ' +
                 "<a href='https://docs.google.com/document/d/1BMoBNpEHWkt56KkY437U5QSCCi6XN2Nu/edit?usp=sharing&ouid=109174770860598202689&rtpof=true&sd=true'>Заявление на открытие счета</a>",{parse_mode: "HTML"})
         }
 
         if ('Есть Альфа счет' === data) {
-            await bot.sendMessage(chatId, 'Отправьте пожалуйста реквизиты своего счета HR')
+            await bot.sendMessage(chatIdUsr, 'Отправьте пожалуйста реквизиты своего счета HR')
         }
         candidate.order = data
         await candidate.save()
 
-        await bot.sendMessage(chatId, 'Для дальнейшего оформления необходимо прислать HR менеджеру следующий пакет документов:' +
+        await bot.sendMessage(chatIdUsr, 'Для дальнейшего оформления необходимо прислать HR менеджеру следующий пакет документов:' +
             'Фото/сканы:\n' +
             '1.  паспорт;\n' +
             '2.  страховое свидетельство ПФР (зелёная карточка СНИЛС);\n' +
--- models.js
+++ models.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize')
 
 const User =sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
-    chatId: { type: DataTypes.STRING, unique: true },
+    chatId: { type: DataTypes.INTEGER, unique: true, defaultValue: 0 },
     firstName: { type: DataTypes.STRING },
     lastName: { type: DataTypes.STRING },
     userName: { type: DataTypes.STRING, unique: true },
